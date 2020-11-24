81c3d5b7de247feda5ac15f021acd91f56163b3a
kazu-f <kbc19b20@stu.kawahara.ac.jp>
2020-11-24 10:22:24 +0900

    多少修正
    おそらく解放忘れになる場所を直した。

--- MiniEngine/MiniEngine/level/MapChipRender.cpp
+++ MiniEngine/MiniEngine/level/MapChipRender.cpp
@@ -21,14 +21,14 @@ namespace Engine {
 		m_modelRender = NewGO<prefab::ModelRender>(0);
 		//ファイルパス作成。
 		wchar_t filePath[256];
-		swprintf_s(filePath, L"Assets/modelData/%s.tkm", m_renderObjDatas[0].name);		//正直ファイルパスの開始地点があやふや。
+		swprintf_s(filePath, L"Assets/modelData/%s.tkm", m_renderObjDatas[0].name);
 
 		size_t oriSize = wcslen(filePath) + 1;
 		size_t convertedChars = 0;
 		char strConcat[] = "";
 		size_t strConcatSize = (strlen(strConcat) + 1) * 2;
 		const size_t newSize = oriSize * 2;
-		char* nString = new char[newSize + strConcatSize];			//ここは解放忘れにならないか？
+		char* nString = new char[newSize + strConcatSize];
 		wcstombs_s(&convertedChars, nString, newSize, filePath, _TRUNCATE);
 		_mbscat_s((unsigned char*)nString, newSize + strConcatSize, (unsigned char*)strConcat);
 
@@ -36,6 +36,8 @@ namespace Engine {
 		initData.m_fxFilePath = "Assets/shader/model.fx";
 		initData.m_vsEntryPointFunc = "VSMain";
 		initData.m_tkmFilePath = nString;
+		delete[] nString;			//解放しておく。
+
 		m_modelRender->Init(initData);
 		m_modelRender->SetPosition(m_renderObjDatas[0].position);
 		m_modelRender->SetRotation(m_renderObjDatas[0].rotation);
