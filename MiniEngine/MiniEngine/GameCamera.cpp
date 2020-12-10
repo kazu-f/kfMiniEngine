9a07857ad0c2e9f6423ce2456d5ed7b70fb356eb
kazu-f <kbc19b20@stu.kawahara.ac.jp>
2020-12-10 09:51:56 +0900

    車モデルを追加。
    Unityから持ってくるのに苦労した。

--- MiniEngine/MiniEngine/GameCamera.cpp
+++ MiniEngine/MiniEngine/GameCamera.cpp
@@ -3,7 +3,7 @@
 
 bool CGameCamera::Start()
 {
-	g_camera3D->SetPosition({ 0.0f, 50.0f, 100.0f });
+	g_camera3D->SetPosition({ 0.0f, 150.0f, 100.0f });
 	g_camera3D->SetTarget({ 0.0f,50.0f,0.0f });
 
 	const auto& targetPos = g_camera3D->GetTarget();
