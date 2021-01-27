ebe9b0723f9dbf0e658e2721763ad73d4b188dc5
kazu-f <kbc19b20@stu.kawahara.ac.jp>
2021-01-27 12:39:48 +0900

    一部ファイルパスを変数化

---
+++ MiniEngine/MiniEngine/Spectator/SpectatorNames.h
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace {
+	namespace FilePath {
+		const char* shirtMale = "Assets/modelData/Human/shirtMale/shirtMale.tkm";
+		const char* nathanMale = "Assets/modelData/Human/nathanMale/nathanMale.tkm";
+		const char* suitMale = "Assets/modelData/Human/suitMale/suitMale.tkm";
+		const char* sophiaWoman = "Assets/modelData/Human/sophiaWoman/sophiaWoman.tkm";
+		const char* claudiaWoman = "Assets/modelData/Human/claudiaWoman/claudiaWoman.tkm";
+		const char* suitWoman = "Assets/modelData/Human/suitWoman/suitWoman.tkm";
+	}
+	namespace AnimPath {
+		const char* clapAnim = "Assets/animData/Human/Clap.tka";
+
+	}
+}
