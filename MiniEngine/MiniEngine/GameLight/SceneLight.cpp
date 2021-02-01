3a0cac8051517c4a66f3cc47c36f6a189e273a05
kazu-f <kbc19b20@stu.kawahara.ac.jp>
2021-02-01 17:54:25 +0900

    ライト調整

--- MiniEngine/MiniEngine/GameLight/SceneLight.cpp
+++ MiniEngine/MiniEngine/GameLight/SceneLight.cpp
@@ -15,31 +15,31 @@ bool SceneLight::Start()
 	//ライトを用意する。
 	prefab::CDirectionLight* light = NewGO<prefab::CDirectionLight>(0);
 
-	Vector4 color = Vector4::Gray * 1.0f;
-	m_lightDir = { 0.0f,0.0f,-1.0f };
+	Vector4 color = Vector4::Gray * 3.0f;
+	m_lightDir = { 0.0f,-1.0f,-1.0f };
 	//color = { 1.0f,1.0f,0.0f,1.0f };
 	light->SetColor(color);
 	light->SetDirection(m_lightDir);
 
 	m_lightArray.push_back(light);
 
-	prefab::CDirectionLight* light2 = NewGO<prefab::CDirectionLight>(0);
+	//prefab::CDirectionLight* light2 = NewGO<prefab::CDirectionLight>(0);
 
-	m_lightDir = { 0.0f,0.0f,1.0f };
-	//color = { 0.0f,1.0f,1.0f,1.0f };
-	light2->SetColor(color);
-	light2->SetDirection(m_lightDir);
+	//m_lightDir = { 0.0f,0.0f,1.0f };
+	////color = { 0.0f,1.0f,1.0f,1.0f };
+	//light2->SetColor(color);
+	//light2->SetDirection(m_lightDir);
 
-	m_lightArray.push_back(light2);
+	//m_lightArray.push_back(light2);
 
-	prefab::CDirectionLight* light3 = NewGO<prefab::CDirectionLight>(0);
+	//prefab::CDirectionLight* light3 = NewGO<prefab::CDirectionLight>(0);
 
-	m_lightDir = { 0.0f,-1.0f,0.0f };
-	//color = { 0.0f,1.0f,1.0f,1.0f };
-	light3->SetColor(color);
-	light3->SetDirection(m_lightDir);
+	//m_lightDir = { 0.0f,-1.0f,0.0f };
+	////color = { 0.0f,1.0f,1.0f,1.0f };
+	//light3->SetColor(color);
+	//light3->SetDirection(m_lightDir);
 
-	m_lightArray.push_back(light3);
+	//m_lightArray.push_back(light3);
 
 	return true;
 }
