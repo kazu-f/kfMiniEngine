7131a56b88dd0dfac52eda6a10124a9c4f869907
kazu-f <kbc19b20@stu.kawahara.ac.jp>
2021-03-16 10:33:21 +0900

    ライトを増やした。影も多少いじった。

--- MiniEngine/MiniEngine/GameLight/SceneLight.cpp
+++ MiniEngine/MiniEngine/GameLight/SceneLight.cpp
@@ -4,6 +4,7 @@
 
 namespace {
 	const Vector3 AMBIENT_LIGHT = { 2.0f,2.0f,2.0f };
+	const Vector3 SHADOW_DIRECTION = { 0.0f,-1.0f,0.0f };
 }
 
 SceneLight::SceneLight()
@@ -30,33 +31,36 @@ bool SceneLight::Start()
 
 	//Vector4 color = Vector4::Gray * 3.0f;
 	Vector4 color = Vector4::White * 5.0f;
-	m_lightDir = { 0.0f,-0.0f,-1.0f };
+	m_lightDir = { 0.0f,-1.0f,-1.0f };
 	//color = { 1.0f,1.0f,0.0f,1.0f };
 	light->SetColor(color);
 	light->SetDirection(m_lightDir);
 
 	m_lightArray.push_back(light);
 
-	//prefab::CDirectionLight* light2 = NewGO<prefab::CDirectionLight>(0);
+	prefab::CDirectionLight* light2 = NewGO<prefab::CDirectionLight>(0);
 
-	//m_lightDir = { 0.0f,0.0f,1.0f };
-	////color = { 0.0f,1.0f,1.0f,1.0f };
-	//light2->SetColor(color);
-	//light2->SetDirection(m_lightDir);
+	m_lightDir = { -1.0f,-1.0f,0.0f };
+	//color = { 0.0f,1.0f,1.0f,1.0f };
+	light2->SetColor(color);
+	light2->SetDirection(m_lightDir);
 
-	//m_lightArray.push_back(light2);
+	m_lightArray.push_back(light2);
 
-	//prefab::CDirectionLight* light3 = NewGO<prefab::CDirectionLight>(0);
+	prefab::CDirectionLight* light3 = NewGO<prefab::CDirectionLight>(0);
 
-	//m_lightDir = { 0.0f,-1.0f,0.0f };
-	////color = { 0.0f,1.0f,1.0f,1.0f };
-	//light3->SetColor(color);
-	//light3->SetDirection(m_lightDir);
+	m_lightDir = { 0.0f,-1.0f,0.0f };
+	//color = { 0.0f,1.0f,1.0f,1.0f };
+	light3->SetColor(color);
+	light3->SetDirection(m_lightDir);
 
-	//m_lightArray.push_back(light3);
+	m_lightArray.push_back(light3);
 
 	//ŠÂ‹«Œõ‚ÌÝ’èB
 	g_graphicsEngine->GetLightManager()->SetAmbientLight(AMBIENT_LIGHT);
+
+	//‰e‚Ì—Ž‚¿‚éŒü‚«‚ðÝ’èB
+	g_graphicsEngine->GetShadowMap()->SetLightDirection(SHADOW_DIRECTION);
 
 	return true;
 }
--- MiniEngine/MiniEngine/graphics/GraphicsConfig.h
+++ MiniEngine/MiniEngine/graphics/GraphicsConfig.h
@@ -18,7 +18,7 @@ namespace Engine {
 		int		shadowMapWidth = 2048;			//シャドウマップの幅。
 		int		shadowMapHeight = 2048;			//シャドウマップの高さ。
 		float	lightHeight = 1000.0f;			//ライトの高さ。
-		float	depthOffset[NUM_SHADOW_MAP] = { 0.0001f , 0.0001f , 0.0002f };	//!<シャドウオフセット。この値を大きくすると、影が落ちにくくなる。
+		float	depthOffset[NUM_SHADOW_MAP] = { 0.0002f , 0.0002f , 0.0002f };	//!<シャドウオフセット。この値を大きくすると、影が落ちにくくなる。
 		float	shadowAreas[NUM_SHADOW_MAP] = { 2000.0f ,4000.0f , 8000.0f };			//影が落ちる範囲。
 		//float	offsetTexelWorld = 0.05f;			//!<ブラーの時にフェッチするオフセット座標。ワールド空間の量。値が大きいほど大きくボケる。
 	};
