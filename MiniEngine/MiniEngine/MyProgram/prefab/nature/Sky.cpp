b9115f0b8b4ef5d7c39fe5dfec9b987842a05c0d
kazu-f <kbc19b20@stu.kawahara.ac.jp>
2021-05-12 12:36:24 +0900

    Skyクラスの実装を始める。

---
+++ MiniEngine/MiniEngine/MyProgram/prefab/nature/Sky.cpp
@@ -0,0 +1,46 @@
+#include "stdafx.h"
+#include "Sky.h"
+
+namespace Engine {
+	namespace prefab {
+
+
+
+		CSky::CSky()
+		{
+		}
+
+		CSky::~CSky()
+		{
+		}
+
+		void CSky::OnDestroy()
+		{
+			DeleteGO(m_modelRender);
+		}
+
+		bool CSky::Start()
+		{
+			////モデルレンダー。
+			//m_modelRender = NewGO<prefab::ModelRender>(0);
+			//m_modelData.m_tkmFilePath = m_skyModelFilePath.c_str();
+
+			//m_modelRender->Init(m_modelData);
+
+			//if (m_modelRender->IsStart())
+			//{
+			//	m_skyCube.InitFromDDSFile(m_skyCubeMapFilePath.c_str());
+
+			//}
+
+			return true;
+		}
+
+		void CSky::Update()
+		{
+		}
+
+
+	}//namespace prefab;
+}//namespace Engine;
+
